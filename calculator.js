d07e49824ae0bdde55ff6e67211ec98692acd94b
Leonard Busenah <busenah@gmail.com>
2019-02-18 08:11:29 -0500

    testing operations

--- calculator.js
+++ calculator.js
@@ -24,3 +24,9 @@ let myCalculator = {
     return `the value is ${answer}`;
   }
 };
+
+console.log(mycalculator.addition(10, 3));
+console.log(mycalculator.substraction(10, 3));
+console.log(mycalculator.multiplication(10, 3));
+console.log(mycalculator.division(10, 3));
+console.log(mycalculator.exponential(10, 3));
